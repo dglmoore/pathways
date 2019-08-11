bb1383d4d08ae765315fea4de3084b97ee5cbe0d
Douglas G. Moore <doug@dglmoore.com>
2019-08-10 22:34:04 -0700

    Move pathways specialization on strings to a separate header

--- src/entropy.cpp
+++ src/entropy.cpp
@@ -1,39 +1,7 @@
-#include <chrono>
-#include <iomanip>
-#include <iostream>
 #include <mgl2/mgl.h>
-#include "../include/pathways.h"
 #include "args.h"
 #include "random.h"
-
-namespace pathways {
-    template <>
-    struct disassembly_type<std::string> {
-        using value = std::vector<Components<std::string>>;
-    };
-
-    template <>
-    auto is_basic<std::string>(std::string const& str) -> bool {
-        return std::size(str) == 1;
-    }
-
-    template <>
-    auto is_below<std::string>(std::string const& x, std::string const& y) -> bool {
-        return y.find(x) != std::string::npos;
-    }
-
-    template <>
-    auto disassemble<std::string>(std::string const& str) -> std::vector<Components<std::string>> {
-        if (str.empty()) {
-            throw std::invalid_argument("string is empty");
-        }
-        auto parts = std::vector<Components<std::string>>{};
-        for (size_t i = 1, len = std::size(str); i < len; ++i) {
-            parts.emplace_back(str.substr(0, i), str.substr(i));
-        }
-        return parts;
-    }
-}
+#include "string.h"
 
 struct Range {
     double min;
--- src/string.cpp
+++ src/string.cpp
@@ -1,36 +1,7 @@
-#include <string>
 #include <iostream>
 #include "../include/pathways.h"
 #include "random.h"
-
-namespace pathways {
-    template <>
-    struct disassembly_type<std::string> {
-        using value = std::vector<Components<std::string>>;
-    };
-
-    template <>
-    auto is_basic<std::string>(std::string const& str) -> bool {
-        return std::size(str) == 1;
-    }
-
-    template <>
-    auto is_below<std::string>(std::string const& x, std::string const& y) -> bool {
-        return y.find(x) != std::string::npos;
-    }
-
-    template <>
-    auto disassemble<std::string>(std::string const& str) -> std::vector<Components<std::string>> {
-        if (str.empty()) {
-            throw std::invalid_argument("string is empty");
-        }
-        auto parts = std::vector<Components<std::string>>{};
-        for (size_t i = 1, len = std::size(str); i < len; ++i) {
-            parts.emplace_back(str.substr(0, i), str.substr(i));
-        }
-        return parts;
-    }
-}
+#include "string.h"
 
 auto main() -> int {
     std::random_device rd;
